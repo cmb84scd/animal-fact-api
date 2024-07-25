ab3e92ee04737d8f2802583f53ec08747eb340c3
Catriona Bennett <catrionamairi@blueyonder.co.uk>
2024-07-25 15:01:29 +0100

    Add functionality to retrieve random fact

--- animal_fact_api/facts/urls.py
+++ animal_fact_api/facts/urls.py
@@ -5,6 +5,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("facts/", views.FactList.as_view()),
-    path("facts/<int:pk>/", views.FactDetail.as_view()),
+    path("facts/all", views.FactList.as_view(), name="fact-list"),
+    path("facts/<int:pk>/", views.FactDetail.as_view(), name="fact-detail"),
+    path("facts/", views.FactDetail.as_view(), name="fact-random"),
 ]
--- animal_fact_api/facts/views.py
+++ animal_fact_api/facts/views.py
@@ -1,6 +1,8 @@
 """Views for animal facts api."""
 
+from django.db.models.functions import Random
 from rest_framework import generics
+from rest_framework.response import Response
 
 from .models import Fact
 from .serializers import FactSerializer
@@ -18,3 +20,12 @@ class FactDetail(generics.RetrieveAPIView):
 
     queryset = Fact.objects.all()
     serializer_class = FactSerializer
+
+    def get(self, request, *args, **kwargs):
+        """Get a specific or random fact."""
+        if "pk" in kwargs:
+            return self.retrieve(request, *args, **kwargs)
+        else:
+            random_record = Fact.objects.order_by(Random()).first()
+            serializer = self.get_serializer(random_record)
+            return Response(serializer.data)

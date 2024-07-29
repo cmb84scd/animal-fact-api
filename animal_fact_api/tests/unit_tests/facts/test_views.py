3a2b9fde5d6a53af96d97df5e9cbea8b4d201ee5
Catriona Bennett <catrionamairi@blueyonder.co.uk>
2024-07-29 14:55:29 +0100

    Fix test so that it tests correct code

--- animal_fact_api/tests/unit_tests/facts/test_views.py
+++ animal_fact_api/tests/unit_tests/facts/test_views.py
@@ -4,8 +4,8 @@ from ....facts.models import Fact
 
 class TestFactView(TestCase):
     def test_returns_single_fact_when_pk_passed_in(self):
-        Fact.objects.create(animal="cat", fact="A cat fact.")
-        request = RequestFactory().get("/facts/1")
-        response = FactDetail.as_view()(request)
+        fact = Fact.objects.create(animal="cat", fact="Some cat fact.")
+        request = RequestFactory().get(f"/facts/{fact.pk}")
+        response = FactDetail.as_view()(request, pk=fact.pk)
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "A cat fact.")
+        self.assertContains(response, "Some cat fact.")

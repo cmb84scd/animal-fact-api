34071f9a207fbcffcd6f0f70c1953411aecf9649
Catriona Bennett <catrionamairi@blueyonder.co.uk>
2024-07-29 15:53:49 +0100

    Refactor view tests to add setUp method

--- animal_fact_api/tests/unit_tests/facts/test_views.py
+++ animal_fact_api/tests/unit_tests/facts/test_views.py
@@ -3,17 +3,23 @@ from ....facts.views import FactDetail
 from ....facts.models import Fact
 
 class TestFactView(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+        Fact.objects.create(animal="dog", fact="A dog fact.")
+        self.fact2 = Fact.objects.create(animal="cat", fact="Some cat fact.")
+        Fact.objects.create(animal="cat", fact="Best cat fact.")
+
     def test_returns_single_fact_when_pk_passed_in(self):
-        fact = Fact.objects.create(animal="cat", fact="Some cat fact.")
-        request = RequestFactory().get(f"/facts/{fact.pk}")
-        response = FactDetail.as_view()(request, pk=fact.pk)
+        request = self.factory.get(f"/facts/{self.fact2.pk}")
+        response = FactDetail.as_view()(request, pk=self.fact2.pk)
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "Some cat fact.")
 
     def test_returns_random_fact_when_no_pk_passed_in(self):
-        Fact.objects.create(animal="cat", fact="Some cat fact.")
-        Fact.objects.create(animal='dog', fact='A dog fact.')
-        request = RequestFactory().get('/facts')
+        request = self.factory.get("/facts")
         response = FactDetail.as_view()(request)
         self.assertEqual(response.status_code, 200)
-        self.assertIn(response.data['fact'], ["Some cat fact.", "A dog fact."])
+        self.assertIn(
+            response.data["fact"],
+            ["A dog fact.", "Some cat fact.", "Best cat fact."]
+        )

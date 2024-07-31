db5c4d67ac46a61a59afd239a7f4ad97a2564e12
Catriona Bennett <catrionamairi@blueyonder.co.uk>
2024-07-31 12:40:43 +0100

    Refactor view tests

--- animal_fact_api/tests/unit_tests/facts/test_views.py
+++ animal_fact_api/tests/unit_tests/facts/test_views.py
@@ -7,27 +7,26 @@ from ....facts.views import FactDetail
 class TestFactView(TestCase):
     def setUp(self):
         self.factory = RequestFactory()
-        Fact.objects.create(animal="dog", fact="A dog fact.")
-        self.fact2 = Fact.objects.create(animal="cat", fact="Some cat fact.")
-        Fact.objects.create(animal="cat", fact="Best cat fact.")
 
     def test_returns_single_fact_when_pk_passed_in(self):
-        request = self.factory.get(f"/facts/{self.fact2.pk}")
-        response = FactDetail.as_view()(request, pk=self.fact2.pk)
+        fact = Fact.objects.create(animal="cat", fact="Some cat fact.")
+        request = self.factory.get(f"/facts/{fact.pk}")
+        response = FactDetail.as_view()(request, pk=fact.pk)
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "Some cat fact.")
 
     def test_returns_random_fact_when_no_pk_passed_in(self):
+        Fact.objects.create(animal="dog", fact="A dog fact.")
+        Fact.objects.create(animal="cat", fact="Best cat fact.")
         request = self.factory.get("/facts")
         response = FactDetail.as_view()(request)
         self.assertEqual(response.status_code, 200)
         self.assertIn(
             response.data["fact"],
-            ["A dog fact.", "Some cat fact.", "Best cat fact."],
+            ["A dog fact.", "Best cat fact."],
         )
 
     def test_returns_no_record_response_when_table_is_empty(self):
-        Fact.objects.all().delete()
         request = self.factory.get("/facts")
         response = FactDetail.as_view()(request)
         self.assertEqual(response.status_code, 404)

1493ca88b61764f9457d19575246f0c8bd2a488e
Catriona Bennett <61843502+cmb84scd@users.noreply.github.com>
2024-12-20 16:21:01 +0000

    Deployment updates (#12)
    * Remove decouple, update deps and safety policy
    
    * Further update to safety policy
    
    * Update settings file and add procfile
    
    * Update procfile and add koyeb config
    
    * Delete koyeb config, amend procfile and add requirements
    
    * Amend python version
    
    * Remove procfile
    
    * Change python version back to 3.12
    
    * Re-add decouple and amend debug var
    
    * Update requirements file

--- animal_fact_api/settings.py
+++ animal_fact_api/settings.py
@@ -25,9 +25,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG", default=True, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config(
+    "DJANGO_ALLOWED_HOSTS", "localhost,127.0.0.1,[::1]"
+).split(",")
 
 
 # Application definition
@@ -45,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -84,8 +87,8 @@ DATABASES = {
         "USER": config("DB_USERNAME"),
         "PASSWORD": config("DB_PASSWORD"),
         "TEST": {},
-        "HOST": "localhost",
-        "PORT": "5432",
+        "HOST": config("DB_HOST", "localhost"),
+        "OPTIONS": {"sslmode": "require"},
     }
 }
 
@@ -133,6 +136,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
